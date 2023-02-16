1646e18d66d798e834495386f0a094110b95bf56
eatrabyo <itthisak.prk@gmail.com>
2023-02-16 15:32:00 +0700

    -list and dict for rename or slice

---
+++ list_dict.py
@@ -0,0 +1,22 @@
+rename_th_expend = {'Enter-':'Entertainment', 'Sight-':'Sight Seeing', 'Accom-':'Accomodation', 'Food &':'Food & Beverage',
+       'Local':'Local Transport', 'Medical':'Medical Care', 'Miscel-':'Miscellaneous'}
+
+region_list = ['East Asia','Asean','Europe','East Europe','The Americas','South Asia','Oceania','Middle East','Africa']
+
+region_dict = {'East Asia':['Asean','Brunei','Cambodia','Indonesia','Laos','Malaysia','Myanmar','Philippines','Singapore','Vietnam','China','Hong Kong','Japan','Korea','Taiwan']
+,'Europe':['Austria','Belgium','Denmark','Finland','France','Germany','Italy','Netherlands','Norway','Russia','Spain','Sweden','Switzerland','United Kingdom','East Europe']
+,'The Americas':['Argentina','Brazil','Canada','USA']
+,'South Asia':['Bangladesh','India','Nepal','Pakistan','Sri Lanka']
+,'Oceania':['Australia','New Zealand']
+,'Middle East':['Egypt','Israel','Kuwait','Saudi Arabia','U.A.E.']
+,'Africa':['S.Africa']}
+
+rename_country_dict = {'ASEAN':'Asean','East  Asia':'East Asia',
+'United  Kingdom':'United Kingdom',
+'East  Europe':'East Europe',
+'The  Americas':'The Americas',
+'South  Asia':'South Asia',
+'Sri  Lanka':'Sri Lanka',
+'New  Zealand':'New Zealand',
+'Middle  East':'Middle East',
+'Saudi  Arabia':'Saudi Arabia'}
